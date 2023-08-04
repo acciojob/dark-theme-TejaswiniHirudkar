e62f869fd7d59e121dd2938dc00193929fde29d9
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-08-04 23:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+function swapTheme() {
+      var appDiv = document.getElementById("app");
+      var swapButton = document.getElementById("swap");
 
+      // Toggle the 'day' and 'night' classes of the main div
+      if (appDiv.classList.contains("day")) {
+        appDiv.classList.remove("day");
+        appDiv.classList.add("night");
+      } else {
+        appDiv.classList.remove("night");
+        appDiv.classList.add("day");
+      }
+
+      // Toggle the 'button_day' and 'button_night' classes of the button
+      if (swapButton.classList.contains("button_day")) {
+        swapButton.classList.remove("button_day");
+        swapButton.classList.add("button_night");
+      } else {
+        swapButton.classList.remove("button_night");
+        swapButton.classList.add("button_day");
+      }
+    }
